880edadee5253d1dc942eed93026e7583417ab07
AngelinaJdN <angelina.julie.dona@gmail.com>
2022-06-22 18:24:47 +0200

    Created a US phone number validator
    Exercise made for learning JS.

---
+++ US-phone-number-validator.js
@@ -0,0 +1,12 @@
+// Function that takes a String and returns a Boolean depending on if the
+// String is a valid US phone number or not. Part of FreeCodeCamp projects.
+
+function telephoneCheck(str) {
+  let withoutParenthesis = /^(1 ?)?([0-9]{3}(-| )?)([0-9]{3}(-| )?)([0-9]{4})$/;
+  let withParenthesis = /^(1 ?)?[(]([0-9]{3}[)](-| )?)([0-9]{3}(-| )?)([0-9]{4})$/;
+
+  if (withoutParenthesis.test(str) | withParenthesis.test(str)) {
+    return true;
+  }
+  else return false;
+}
