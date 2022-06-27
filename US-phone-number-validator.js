9c58904ceb3ccf7a05abe7aecb7ca5f45e9b011d
AngelinaJdN <angelina.julie.dona@gmail.com>
2022-06-27 21:05:56 +0200

    Improved legibility of US phone number validator

--- US-phone-number-validator.js
+++ US-phone-number-validator.js
@@ -2,8 +2,8 @@
 // String is a valid US phone number or not. Part of FreeCodeCamp projects.
 
 function telephoneCheck(str) {
-  let withoutParenthesis = /^(1 ?)?([0-9]{3}(-| )?)([0-9]{3}(-| )?)([0-9]{4})$/;
-  let withParenthesis = /^(1 ?)?[(]([0-9]{3}[)](-| )?)([0-9]{3}(-| )?)([0-9]{4})$/;
+  let withoutParenthesis = /^(1 ?)?(\d{3}(-| )?)(\d{3}(-| )?)(\d{4})$/;
+  let withParenthesis = /^(1 ?)?\((\d{3}\)(-| )?)(\d{3}(-| )?)(\d{4})$/;
 
   if (withoutParenthesis.test(str) || withParenthesis.test(str)) {
     return true;
